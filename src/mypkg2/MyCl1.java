36010654e78f14394c4b94abdb5ecf17c74d0286
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-09-18 13:44:07 +0530

    jdbc

---
+++ src/mypkg2/MyCl1.java
@@ -0,0 +1,22 @@
+
+package mypkg2;
+
+import java.util.Scanner;
+
+
+
+public class MyCl1 {
+
+
+    public static void main(String[] args) {
+Scanner sc = new Scanner (System.in);
+int a , b, c;
+System.out.println("input no. ");
+a=sc.nextInt();
+System.out.println("input no. ");
+b=sc.nextInt();
+c=a+b;
+System.out.println("sum "+c);
+    }
+    
+}
